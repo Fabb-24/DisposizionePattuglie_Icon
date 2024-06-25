10aff8cba9fd8a0a45792f91e6b9f4aad696698d
Fabb-24 <cirullofabio24@gmail.com>
2024-06-25 17:30:13 +0200

    sistemati il file index e il file readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # DisposizionePattuglie_Icon
+
+## Librerie Python
+
+Per l'esecuzione del programma è necessario installare delle specifiche libreria python elencate nel file "requirements.txt" presente nel percorso /src.  
+Per installare velocemente tutte le librerie occorre aprire una istanza del prompt dei comandi e digitare la seguente stringa:
+```
+pip install -r <percorso requirements.txt>
+```
+dove al posto di \<percorso requirements.txt> va inserito il percorso assoluto del file indicato.  
+
+## SWI-Prolog
+
+Il progetto fa uso del linguaggio Prolog. Per poterlo utilizzare occorre scaricare anche il programma SWI-Prolog dalla pagina di download del sito ufficiale:  
+`https://www.swi-prolog.org/Download.html`  
+  
+Se nonostante l'installazione si riscontrano problemi nell'esecuzione del programma dovuti a SWI-Prolog si consiglia di provare a installare una versione meno recente.  
+
+## Esecuzione del programma
+
+Per eseguire il programma bisogna eseguire il file "index.py" presente nel percorso /src.  
+  
+Verrà chiesto inizialmente se si vuole eseguire l'apprendimento supervisionato o utilizzare dei modelli già addestrati nel caso fossero presenti. Se non sono presenti modelli nell'apposito percorso si è obbligati a crearne facendo l'apprendimento supervisionato.  
+Dopo verrà richiesto il modello da utilizzare per le predizioni, facendo scegliere tra quelli disponibili. In più verranno chieste informazioni riguardanti la settimana, il giorno e la fascia oraria.  
+Una volta inserite le informazioni necessarie, inizierà la risoluzione del problema di ottimizzazione che darà come risultato la lista delle aree a cui assegnare la pattuglia.  
+La durata del problema di ottimizzazione è variabile e dipende dalle informazioni inserite.
--- src/index.py
+++ src/index.py
@@ -7,24 +7,61 @@ import os
 
 
 if __name__ == "__main__":
-    # Pulizia dei dataset
+    # Percorso contenente i modelli di apprendimento utilizzabili
+    modelsPath = os.path.join(getBasePath(), "learning", "models")
+    models = os.listdir(modelsPath)
+
+    # Pulizia del dataset delle aree di Chicago
     chicagoAreasDf = cleanChicagoAreas()
-    chicagoCrimesDf = cleanChicagoCrimes()
 
-    # Decommentare per eseguire l'apprendimento supervisionato
-    '''# Apprendimento supervisionato per la previsione della gravità dei crimini
-    supervisedLearning = SL(chicagoCrimesDf, "Severity")
-    # Se come parametro si passa il json con i parametri verrano usati invece che cercati
-    res = supervisedLearning.trainModel(os.path.join(getBasePath(), "learning"))
-    printResults(res)'''
+    # Scelta tra apprendimento supervisionato e utilizzo di un modello già addestrato
+    while True:
+        print("Scelta:\n1. Eseguire l'apprendimento supervisionato\n2. Utilizzare un modello già addestrato precedentemente\n")
+        choice = int(input("Inserire la scelta: "))
+        if choice != 1 and choice != 2:
+            print("Scelta non valida\n")
+        elif choice == 2 and len(models) == 0:
+            print("Non sono presenti modelli di apprendimento precedentemente addestrati\n")
+        else:
+            break
     
+    if choice == 1:
+        print("\n--- Apprendimento supervisionato ---\n\n")
+        # Pulizia del dataset dei crimini di Chicago
+        chicagoCrimesDf = cleanChicagoCrimes()
+        # Apprendimento supervisionato per la previsione della gravità dei crimini
+        supervisedLearning = SL(chicagoCrimesDf, "Severity")
+        # Se come parametro si passa il json con i parametri verrano usati invece che cercati
+        res = supervisedLearning.trainModel(os.path.join(getBasePath(), "learning"))
+        printResults(res)
+        print("\nApprendimento supervisionato completato")
+    
+    # Disposizione delle pattuglie
+    print("\n\n--- Ricerca della disposizione migliore delle pattuglie ---\n\n")
+    # scelta del modello di apprendimento da utilizzare. Vengono proposti i modelli disponibili nel percorso /learning/models
+    while True:
+        print("Scegli il modello di apprendimento da utilizzare:")
+        modelsPath = os.path.join(getBasePath(), "learning", "models")
+        models = os.listdir(modelsPath)
+        for i, model in enumerate(models):
+            print(f"{i+1}. {model}")
+        print("\nModello scelto:", end=" ")
+        choice = int(input())
+        if choice < 1 or choice > len(models):
+            print("Scelta non valida\n")
+        else:
+            break
 
-    # Creazione della knowledge base
-    week = 32
-    day = 2
-    hour = 1
+    print("\nInserire il numero della settimana (1-52):", end=" ")
+    week = int(input())
+    print("Inserire il giorno della settimana (1-7): ", end=" ")
+    day = int(input())
+    print("Inserire l'ora del giorno (0-23): ", end=" ")
+    hour = int(input())
+    print("\n")
+    # Creazione della base di conoscenza
     kb = KB(chicagoAreasDf, os.path.join(getBasePath(), "learning", "models", "DecisionTree.pkl"), week, day, hour)
-
+    # Problema di ottimizzazione per la ricerca della disposizione
     pa = PA(kb)
     sol = pa.findBestArrangement()
     printSolution(sol)
